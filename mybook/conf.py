3bac0865d4be285a0a8b256d9ab7786f95f3c611
sulthankarimov <sulthankarimov@gmail.com>
2024-06-16 15:11:18 +0700

    update

--- mybook/conf.py
+++ mybook/conf.py
@@ -13,6 +13,7 @@ external_toc_path = '_toc.yml'
 html_baseurl = ''
 html_favicon = ''
 html_logo = 'logo.png'
+html_sidebars = {'**': ['ablog/postcard.html', 'ablog/tagcloud.html', 'ablog/recentposts.html', 'ablog/categories.html']}
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
 html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': '', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'docs', 'repository_url': 'https://github.com/sakarimov/My Book', 'repository_branch': 'main', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'analytics': {'google_analytics_id': ''}, 'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True}
